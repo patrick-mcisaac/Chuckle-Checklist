bde1b2cc47f5d720baa6a2f14451e4be6e22ba16
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-13 09:48:12 -0500

    I think i stored state correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css"
+import { useState } from "react"
 
 export const App = () => {
-  return <div>Hello World!</div>
+	const [userInput, setUserInput] = useState()
+
+	return (
+		<>
+			<div>Hello World!</div>
+			<input
+				className="p-5 text-2xl"
+				type="text"
+				placeholder="New One Liner"
+				onChange={e => setUserInput(e.target.value)}
+			/>
+		</>
+	)
 }
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,1 +1,21 @@
 // on part 2 of chuckle creating a new one liner
+import "../App.css"
+
+// export function
+export const UserInput = () => {
+	//  store the users input with useState()
+	const [userInput, setUserInput] = useState("")
+
+	//  return a text input
+	return (
+		<>
+			<input
+				className="p-5 text-2xl"
+				type="text"
+				placeholder="New One Liner"
+				onChange={e => console.log(e)}
+			/>
+		</>
+	)
+}
+// create function to pass into onChange
